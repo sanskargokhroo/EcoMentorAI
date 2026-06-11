9faf1b8e8a7bb2b14a7fbd71177e13bb0c78c4a3
sanskargokhroo <sanskargokhroo1@gmail.com>
2026-06-11 15:37:45 +0530

    Fix unclickable button caused by overlapping SVG curve

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -150,7 +150,7 @@ const Landing: React.FC = () => {
         </div>
         
         {/* Beautiful Deep SVG Curve at the bottom */}
-        <div className="absolute bottom-0 left-0 w-full overflow-hidden leading-none z-20">
+        <div className="absolute bottom-0 left-0 w-full overflow-hidden leading-none z-20 pointer-events-none">
           <svg className="relative block w-full h-[100px] md:h-[200px]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" preserveAspectRatio="none">
             <path fill="currentColor" fillOpacity="1" d="M0,224L48,208C96,192,192,160,288,160C384,160,480,192,576,213.3C672,235,768,245,864,224C960,203,1056,149,1152,138.7C1248,128,1344,160,1392,176L1440,192L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z" className="text-gray-50 dark:text-gray-900"></path>
           </svg>
